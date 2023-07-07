a49093ebb475a67486b2bc6190b67dafcd9bb5bf
LucasRosa53 <lucasrosa223@hotmail.com>
2023-07-07 19:09:33 -0300

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not permanence_period or not target_time:
+        return None
+
+    students = 0
+
+    for entry_time, exit_time in permanence_period:
+        if not isinstance(entry_time, int) or not isinstance(exit_time, int):
+            return None
+
+        if entry_time <= target_time <= exit_time:
+            students += 1
+
+    return students
