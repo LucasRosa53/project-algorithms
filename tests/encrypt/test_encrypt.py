2f7d7edb9c4f2a924421e4cf9f22fd3a33133ad5
LucasRosa53 <lucasrosa223@hotmail.com>
2023-07-07 20:47:36 -0300

    requisito 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,10 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message('lucasr', 3) == 'cul_rsa'
+    assert encrypt_message('lucasr', 4) == 'rs_acul'
+    assert encrypt_message('lucasr', 9) == 'rsacul'
+    with pytest.raises(TypeError):
+        encrypt_message(3, 'lucas')
